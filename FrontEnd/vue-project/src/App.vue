d7f213dcf6c86fcaf46a82aad30d6b2b9b567cca
balaa <2941930242@qq.com>
2024-10-31 23:20:46 +0800

    修改首页链接的布局

--- FrontEnd/vue-project/src/App.vue
+++ FrontEnd/vue-project/src/App.vue
@@ -5,22 +5,25 @@
     <router-view></router-view>
 
     <div class="router-links"> 
-      <router-link to="/">
-        <el-link type="primary">Home</el-link>   <!-- 使用element组件美化 -->
-      </router-link>
-      <br />
-      <router-link to="/login">
-        <el-link type="primary">Login</el-link>  
-      </router-link>
+      <div class="left-links">
+        <router-link to="/">
+          <el-link type="primary">Home</el-link>   <!-- 使用element组件美化 -->
+        </router-link>
+        <router-link to="/login">
+          <el-link type="primary">Login</el-link>  
+        </router-link>        
+      </div>
+     
       <!-- 添加登录链接 -->
-      <br />
-      <router-link to="/app">
-        <el-link type="primary">App</el-link>  
-      </router-link>
-      <br />
-      <router-link to="/user" >
-       <el-link type="primary">UserProfile</el-link>  
-      </router-link>
+      <div class="right-links">
+          <router-link to="/app">
+            <el-link type="primary">App</el-link>  
+          </router-link>
+          <router-link to="/user" >
+            <el-link type="primary">UserProfile</el-link>  
+          </router-link>
+      </div>
+
     </div>
   </div>
 </template>
@@ -33,6 +36,18 @@
 }
 
 .router-links {
-  margin-top: auto; /* 将链接推到底部 */
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  bottom: 20px; /* 距离底部 */
+  width: 100%;
+  padding: auto;
+}
+
+
+.left-links,
+.right-links {
+  display: flex;
+  gap: 10px;
 }
 </style>
