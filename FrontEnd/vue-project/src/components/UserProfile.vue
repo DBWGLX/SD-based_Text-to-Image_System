3c23c61e63cd406de5b9973f31c56905af52fb1e
lyh <3373808338@qq.com>
2024-10-31 21:27:01 +0800

    更新用户信息界面

--- FrontEnd/vue-project/src/components/UserProfile.vue
+++ FrontEnd/vue-project/src/components/UserProfile.vue
@@ -1,45 +1,118 @@
 <template>
-  <div class="container">
-    <nav class="side-menu">
-      <ul>
-        <li :class="{ active: activeTab === 'account-settings' }" @click="activeTab = 'account-settings'">
-          <a href="#/account-settings"><i class="fas fa-cog"></i>账户设置</a>
-        </li>
-      </ul>
-    </nav>
-    
-    <main class="content-area">
-      <header class="page-header">
-        <h2>用户中心</h2>
-        <button class="close-button" @click="$emit('close')">关闭</button>
-      </header>
-      
-      <section class="profile-section" v-if="activeTab === 'account-settings'">
-        <div class="avatar-container">
-          <img :src="userInfo.avatar" alt="User Avatar" />
-          <label for="avatar-upload" class="upload-label">修改头像</label>
-          <input type="file" id="avatar-upload" accept=".jpg,.jpeg,.png,.svg" @change="handleAvatarUpload" />
-        </div>
-        
-        <dl class="profile-details">
-          <dt>姓名</dt>
-          <dd>{{ userInfo.name }} <span>(微信用户{{ userInfo.weixinId }})</span></dd>
-          
-          <dt>用户ID</dt>
-          <dd>{{ userInfo.userId }}</dd>
-          
-          <dt>手机</dt>
-          <dd>{{ userInfo.phone ? userInfo.phone : '未绑定' }}</dd>
-          
-          <dt>微信</dt>
-          <dd>{{ userInfo.isWeixinBound ? '已绑定' : '未绑定' }}</dd>
-          
-          <dt>邮箱</dt>
-          <dd>{{ userInfo.email ? userInfo.email : '未绑定' }}</dd>
-        </dl>
-      </section>
-      
-    </main>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <el-menu router :default-active="$route.path" mode="vertical">
+          <el-menu-item index="/account-settings">
+            <span>
+              <i class="el-icon-setting"></i>
+              <el-text class="mx-1" type="info">账户设置</el-text>
+            </span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-header>
+          用户中心
+        </el-header>
+
+        <el-main>
+          <div class="profile-section">
+            <div class="avatar-container">
+              <!-- 头像 -->
+              <img  style="width: 150px; height: 150px;">
+              <el-button plain class="edit-avatar-btn">修改头像</el-button>
+            </div>
+
+            <el-divider border-style="double" />
+
+            <div class="profile-details">
+              <div class="profile-item">
+                <div class="item-title">
+                  <el-text class="mx-1">姓名</el-text>
+                </div>
+                <div class="item-value">{{ userInfo.name }}</div>
+                <el-button plain class="edit-btn">编辑</el-button>
+              </div>
+
+              <el-divider border-style="double" />
+
+              <div class="profile-item">
+                <div class="item-title">
+                  <el-text class="mx-1">用户ID</el-text>
+                </div>
+                <div class="item-value">{{ userInfo.id }}</div>
+                <el-button plain class="copy-btn">复制</el-button>
+              </div>
+
+              <el-divider border-style="double" />
+
+              <div class="profile-item">
+                <div class="item-title">
+                  <el-text class="mx-1">手机号</el-text>
+                </div>
+                <div class="item-value">{{ userInfo.phoneNumber }}</div>
+                <el-button plain class="bind1-btn">立即绑定</el-button>
+              </div>
+
+              <el-divider border-style="double" />
+
+              <div class="profile-item">
+                <div class="item-title">
+                  <el-text class="mx-1">微信</el-text>
+                </div>
+                <div class="item-value">{{ userInfo.weChatStatus }}</div>
+                <div class="we-chat-actions">
+                  <el-button plain class="change-btn">更换</el-button>
+                  <el-button plain class="unbind-btn">解绑</el-button>
+                </div>
+              </div>
+
+              <el-divider border-style="double" />
+
+              <div class="profile-item">
+                <div class="item-title">
+                  <el-text class="mx-1">邮箱</el-text>
+                </div>
+                <div class="item-value">{{ userInfo.email }}</div>
+                <el-button plain class="bind2-btn">立即绑定</el-button>
+              </div>
+
+              <el-divider border-style="double" />
+
+              <div class="profile-item">
+                <div class="item-title">
+                  <el-text class="mx-1">实名认证</el-text>
+                </div>
+                <div class="item-value">{{ userInfo.authentication }}</div>
+                <el-button plain class="authen-btn">认证</el-button>
+              </div>
+
+              <el-divider border-style="double" />
+
+              <div class="profile-item">
+                <div class="item-title">
+                  <el-text class="mx-1">密码</el-text>
+                </div>
+                <div class="item-value">{{ userInfo.passward }}</div>
+                <el-button plain class="passward-btn">设置密码</el-button>
+              </div>
+
+              <el-divider border-style="double" />
+
+              <div class="profile-item">
+                <div class="item-title">
+                  <el-text class="mx-1">账号注销</el-text>
+                </div>
+                <div class="item-value">{{ userInfo.cancellation }}</div>
+                <el-button type="danger" plain class="cancel-btn">注销</el-button>
+              </div>
+            </div>
+          </div>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -47,115 +120,116 @@
 export default {
   data() {
     return {
-      activeTab: 'account-settings',
-      userInfo: {},
+      userInfo: {
+        name: "微信用户OquH2t8F",
+        id: "29988916",
+        phoneNumber: "666666",
+        weChatStatus: "已绑定",
+        email: "1234679",
+        authentication: "未认证",
+        passward: "464464",
+        cancellation: "注销后不可恢复，请谨慎操作"
+      }
     };
   },
-  mounted() {
-    this.fetchUserInfo();
-  },
   methods: {
-    fetchUserInfo() {
-      // 获取用户信息，可以是从服务器获取，也可以是模拟数据
-      // 示例：假设用户信息存储在localStorage中
-      const isAuthenticated = localStorage.getItem('isAuthenticated');
-      if (isAuthenticated) {
-        this.userInfo = JSON.parse(localStorage.getItem('userInfo')) || {};
-      }
-    },
-    handleAvatarUpload(event) {
-      // 处理头像上传事件
-      const file = event.target.files[0];
-      if (!file) return;
-
-      // 这里可以添加上传文件到服务器的逻辑
-      console.log('Selected avatar:', file);
-    },
-  },
+    closeSidebar() {
+      // Implement logic to close the sidebar or navigate away.
+    }
+  }
 };
 </script>
 
 <style scoped>
-.container {
-  display: flex;
+.common-layout {
+  min-height: 100vh;
 }
 
-.side-menu {
-  width: 200px;
-  background: #f8fafc;
-  padding: 20px;
-  border-right: 1px solid #ddd;
+.el-container {
+  height: 100%;
 }
 
-.side-menu ul {
-  list-style-type: none;
-  padding: 0;
+.el-aside {
+  background-color: #fff;
 }
 
-.side-menu li {
-  margin-bottom: 10px;
-}
-
-.side-menu li.active {
-  background: #e0e0e0;
-}
-
-.content-area {
-  flex-grow: 1;
-  padding: 20px;
-}
-
-.page-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-}
-
-.close-button {
-  background: transparent;
-  border: none;
-  color: #aaa;
+.el-header {
+  background-color: #fff;
+  line-height: 60px;
+  text-align: center;
+  font-weight: bold;
   font-size: 20px;
-  cursor: pointer;
+}
+
+.el-main {
+  padding: 20px;
+}
+
+.profile-section {
+  max-width: 600px;
+  margin: 0 auto;
 }
 
 .avatar-container {
-  position: relative;
-  width: 100px;
-  height: 100px;
-  overflow: hidden;
-  border-radius: 50%;
+  text-align: center;
   margin-bottom: 20px;
 }
 
-.upload-label {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(0, 0, 0, 0.5);
-  opacity: 0;
-  transition: opacity 0.3s;
-  color: white;
-  cursor: pointer;
+.edit-avatar-btn {
+  float: right;
+  margin-top: 10px;
 }
 
-.upload-label:hover {
-  opacity: 1;
-}
-
-.profile-details dt {
+.item-title {
   font-weight: bold;
-  margin-bottom: 5px;
 }
 
-.profile-details dd {
-  margin-bottom: 10px;
+.item-value {
+  display: inline-block; /* 让值和按钮在同一行 */
+  margin-right: 10px; /* 增加间距 */
 }
+
+.edit-btn{
+  float: right;
+}
+.copy-btn {
+  float: right;
+}
+
+
+.bind1-btn {
+  float : right;
+}
+
+.we-chat-actions {
+  display: flex;
+  justify-content: flex-end; /* 将按钮移到最右边 */
+  gap: 3px; /* 按钮之间的间距 */
+}
+
+/* .bind1-btn, .change-btn {
+  
+} */
+
+.bind2-btn {
+  float: right;
+}
+
+.authen-btn {
+  float: right;
+}
+
+.passward-btn {
+  float: right;
+}
+
+.cancel-btn {
+  float: right;
+} 
+
+.unbind-btn {
+  margin-left: 10px;
+}
+
 
 </style>
