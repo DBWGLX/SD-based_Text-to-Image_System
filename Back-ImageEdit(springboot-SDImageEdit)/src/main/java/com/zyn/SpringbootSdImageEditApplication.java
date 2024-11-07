82dffe8db1e0063e7ba7a5c16b800bf61f1d7159
DBWGLX <1125506557@qq.com>
2024-11-07 13:47:21 +0800

    modifiy names of some folders

--- Back-ImageEdit(springboot-SDImageEdit)/src/main/java/com/zyn/SpringbootSdImageEditApplication.java
+++ Back-ImageEdit(springboot-SDImageEdit)/src/main/java/com/zyn/SpringbootSdImageEditApplication.java
@@ -2,10 +2,12 @@ package com.zyn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
-//@ComponentScan("com.zyn.dao")
+// 启动 Spring Boot 应用的主类
+@SpringBootApplication // 启用组件扫描和自动配置
+@ComponentScan("com.zyn.dao") // 指定要扫描的包
 public class SpringbootSdImageEditApplication {
 
     public static void main(String[] args) {
