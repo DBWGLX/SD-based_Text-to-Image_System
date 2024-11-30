dd00e135fc1fd0b8a774ccb7d23f895bc0139b25
DBWGLX <1125506557@qq.com>
2024-11-30 21:36:23 +0800

    The interceptor intercepted the access to Swagger and the path has been ignored.

--- BackEnd/back-register/src/main/java/com/back/backregister/BackRegisterApplication.java
+++ BackEnd/back-register/src/main/java/com/back/backregister/BackRegisterApplication.java
@@ -1,11 +1,12 @@
 package com.back.backregister;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-
+@MapperScan("com.back.back-register.Mapper")
 @SpringBootApplication
 public class BackRegisterApplication {
 
